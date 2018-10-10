f56a235c2f0ec53607e60d926f6a908b8ca15d12
Dizy903 <Goffart@intechinfo.fr>
2018-10-10 16:51:48 +0200

    [WIP] Initializing CK-DB.

---
+++ ITI.Human.Data/Package.cs
@@ -0,0 +1,20 @@
+﻿using CK.Setup;
+using CK.SqlServer.Setup;
+
+namespace CKS.Data
+{
+    [SqlPackage(
+    ResourcePath = "Res",
+    Schema = "CK",
+    Database = typeof(SqlDefaultDatabase),
+    ResourceType = typeof(Package)),
+    Versions("1.0.0")]
+    public abstract class Package : SqlPackage
+    {
+        void StobjConstruct(
+            )
+        {
+
+        }
+    }
+}
