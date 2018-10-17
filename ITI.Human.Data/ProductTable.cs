5632001bf43f84487eba6ea7c69c32ab7c550c4a
Pierre Viara <viara@intechinfo.fr>
2018-10-17 11:39:51 +0200

    Added ProductTable and its creation stored proc.

---
+++ ITI.Human.Data/ProductTable.cs
@@ -0,0 +1,18 @@
+﻿using CK.Setup;
+using CK.SqlServer;
+using CK.SqlServer.Setup;
+
+namespace ITI.Human.Data
+{
+    [SqlTable("tProduct", Package = typeof(Package))]
+    [Versions("1.0.0")]
+    public abstract class ProductTable : SqlTable
+    {
+        void StObjConstruct()
+        {
+        }
+
+        [SqlProcedure("sProductCreate")]
+        public abstract int Create(ISqlCallContext ctx, int actorId, string name, string desc, int price);
+    }
+}
---
+++ ITI.Human.Data/Res/Model/ITIH.ProductTable.Install.1.0.0.sql
@@ -0,0 +1,11 @@
+﻿create table ITIH.tProduct (
+	ProductId int not null identity(0, 1),
+	[Name] nvarchar(256) not null,
+	[Desc] nvarchar(512) not null,
+	Price int not null
+
+	constraint PK_ITIH_tProduct primary key (ProductId)
+);
+
+insert into ITIH.tProduct ([Name], [Desc], Price)
+	values (N'', N'', 0);
