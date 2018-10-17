ebcc500694acee4f9acc901dec94dee5df9f70dd
Pierre Viara <viara@intechinfo.fr>
2018-10-17 12:39:31 +0200

    [WIP] Added tests on order system.
    Problem when trying to execute tests : FileNotLoadException.

---
+++ Tests/ITI.Human.Data.Tests/OrderSystemTests/OrderedProductTests.cs
@@ -0,0 +1,39 @@
+﻿using CK.SqlServer;
+using FluentAssertions;
+using NUnit.Framework;
+
+using static CK.Testing.DBSetupTestHelper;
+
+namespace ITI.Human.Data.Tests.OrderSystemTests
+{
+    [TestFixture]
+    public class OrderedProductTests
+    {
+        [Test]
+        public void givenNotAdmissibleArguments_whenCreateOrdoredProduct_shouldReturnSqlDetailedException()
+        {
+            var oProductTable = CK.Core.StObjModelExtension.Obtain<OrderedProductTable>(TestHelper.StObjMap.Default);
+
+            using (var ctx = new SqlStandardCallContext())
+            {
+                oProductTable.Invoking(
+                    sut => oProductTable.Create(ctx, 0, -1, -1)
+                )
+                .Should()
+                .Throw<SqlDetailedException>();
+            }
+        }
+
+        [Test]
+        public void givenAdmissibleArguments_whenCreateOrderedProduct_shouldReturnCreatedOrderId()
+        {
+            var oProductTable = CK.Core.StObjModelExtension.Obtain<OrderedProductTable>(TestHelper.StObjMap.Default);
+
+            using (var ctx = new SqlStandardCallContext())
+            {
+                var createdOrderedProduct = oProductTable.Create(ctx, 0, 0, 0);
+                Assert.That(createdOrderedProduct, Is.GreaterThan(0));
+            }
+        }
+    }
+}
---
+++ Tests/ITI.Human.Data.Tests/OrderSystemTests/ProductTests.cs
@@ -0,0 +1,39 @@
+﻿using CK.SqlServer;
+using FluentAssertions;
+using NUnit.Framework;
+
+using static CK.Testing.DBSetupTestHelper;
+
+namespace ITI.Human.Data.Tests.OrderSystemTests
+{
+    [TestFixture]
+    public class ProductTests
+    {
+        [Test]
+        public void givenNotAdmissibleArguments_whenCreateProduct_shouldThrowSqlDetailedException()
+        {
+            var pTable = CK.Core.StObjModelExtension.Obtain<ProductTable>(TestHelper.StObjMap.Default);
+
+            using (var ctx = new SqlStandardCallContext())
+            {
+                pTable.Invoking(
+                    sut => pTable.Create(ctx, 0, null, null, 0)
+                )
+                .Should()
+                .Throw<SqlDetailedException>();
+            }
+        }
+
+        [Test]
+        public void givenAdmissibleArguments_whenCreateProduct_shouldReturnCreatedProductId()
+        {
+            var pTable = CK.Core.StObjModelExtension.Obtain<ProductTable>(TestHelper.StObjMap.Default);
+
+            using (var ctx = new SqlStandardCallContext())
+            {
+                var createdProduct = pTable.Create(ctx, 0, "Kinder Maxi", "A delicious chocolate bar.", 1);
+                Assert.That(createdProduct, Is.GreaterThan(0));
+            }
+        }
+    }
+}
