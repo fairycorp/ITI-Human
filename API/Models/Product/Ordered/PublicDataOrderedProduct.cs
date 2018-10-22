17115a68a9131caac0edeca89728f3bd58214c96
Pierre Viara <viara@intechinfo.fr>
2018-10-22 19:47:15 +0200

    Added models on Product, Order & OrderedProduct.

---
+++ API/Models/Product/Ordered/PublicDataOrderedProduct.cs
@@ -0,0 +1,23 @@
+﻿namespace API.Models.Product.Ordered
+{
+    /// <summary>
+    /// Represents what an ordered product is.
+    /// </summary>
+    public class PublicDataOrderedProduct
+    {
+        /// <summary>
+        /// Ordered product id.
+        /// </summary>
+        public int OrderedProductId { get; set; }
+
+        /// <summary>
+        /// Matching order id.
+        /// </summary>
+        public int OrderId { get; set; }
+
+        /// <summary>
+        /// Matching product id.
+        /// </summary>
+        public int ProductId { get; set; }
+    }
+}
---
+++ API/Models/Product/PublicDataProduct.cs
@@ -0,0 +1,28 @@
+﻿namespace API.Models.Product
+{
+    /// <summary>
+    /// Represents what a product is.
+    /// </summary>
+    public class PublicDataProduct
+    {
+        /// <summary>
+        /// Product id.
+        /// </summary>
+        public int ProductId { get; set; }
+
+        /// <summary>
+        /// Product (unique) name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Product description.
+        /// </summary>
+        public string Desc { get; set; }
+
+        /// <summary>
+        /// Product price ; must not be negative.
+        /// </summary>
+        public int Price { get; set; }
+    }
+}
