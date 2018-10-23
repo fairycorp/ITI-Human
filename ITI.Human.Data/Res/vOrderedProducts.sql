fe55868403320bd3c61f9dcea7c13e4347585ef9
Pierre Viara <viara@intechinfo.fr>
2018-10-23 11:03:43 +0200

    Added vOrderedProducts.

---
+++ ITI.Human.Data/Res/vOrderedProducts.sql
@@ -0,0 +1,10 @@
+﻿create view ITIH.vOrderedProducts
+as
+	select
+		OrderedProductId = op.OrderedProductId,
+		ProductId = p.ProductId,
+		[Name] = p.[Name],
+		[Desc] = p.[Desc],
+		Price = p.Price
+	from tOrderedProduct op
+		join tProduct p on p.ProductId = op.ProductId
