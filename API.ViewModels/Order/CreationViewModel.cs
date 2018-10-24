60d44fcc16bcfd0b118fb74371d91c267eb73a2a
Pierre Viara <viara@intechinfo.fr>
2018-10-24 16:38:14 +0200

    Updated & added a few ViewModels.

---
+++ API.ViewModels/Order/CreationViewModel.cs
@@ -0,0 +1,18 @@
+﻿using API.ViewModels.Product.Ordered;
+using System.Collections.Generic;
+
+namespace API.ViewModels.Order
+{
+    public class CreationViewModel
+    {
+        /// <summary>
+        /// See <see cref="DetailedDataOrder.OrderInfo"/>.
+        /// </summary>
+        public BasicDataOrder Info { get; set; }
+
+        /// <summary>
+        /// See <see cref="DetailedDataOrder.Products"/>.
+        /// </summary>
+        public IEnumerable<BasicDataOrderedProduct> Products { get; set; }
+    }
+}
---
+++ API.ViewModels/Product/Ordered/CreationViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace API.ViewModels.Product.Ordered
+{
+    public class CreationViewModel
+    {
+        public int OrderId { get; set; }
+
+        public int ProductId { get; set; }
+
+        public bool HasBeenDelivered { get; set; }
+    }
+}
