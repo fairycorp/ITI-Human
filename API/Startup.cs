1bc36e9bda2fa696217a7c4b66ae8a4eee8ec99e
Pierre Viara <viara@intechinfo.fr>
2018-10-31 13:06:33 +0100

    [WIP] Setting up GitHub authentication in Startup.

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,12 +1,21 @@
 ﻿using API.Services.Order;
 using API.Services.Product;
+using CK.AspNet.Auth;
+using CK.DB.User.UserGitHub;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OAuth;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Linq;
 using System;
 using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace API
 {
@@ -24,6 +33,49 @@ namespace API
 
         public void ConfigureServices(IServiceCollection services)
         {
+            // This authentication system will be used by mobile app ONLY.
+            services.AddAuthentication(WebFrontAuthOptions.OnlyAuthenticationScheme)
+            .AddWebFrontAuth(options =>
+             {
+                 options.ExpireTimeSpan = TimeSpan.FromHours(1);
+                 options.SlidingExpirationTime = TimeSpan.FromHours(1);
+             }).AddOAuth("GitHub", options =>
+             {
+                 options.ClientId = _configuration["GitHubApp:ClientId"];
+                 options.ClientSecret = _configuration["GitHubApp:ClientSecret"];
+                 options.CallbackPath = new PathString("/signin-github");
+
+                 options.Scope.Add("user:email");
+                 options.AuthorizationEndpoint = "https://github.com/login/oauth/authorize";
+                 options.TokenEndpoint = "https://github.com/login/oauth/access_token";
+                 options.UserInformationEndpoint = "https://api.github.com/user";
+
+                 options.ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "id");
+                 options.ClaimActions.MapJsonKey(ClaimTypes.Name, "name");
+                 options.ClaimActions.MapJsonKey("urn:github:login", "login");
+                 options.ClaimActions.MapJsonKey("urn:github:url", "html_url");
+                 options.ClaimActions.MapJsonKey("urn:github:avatar", "avatar_url");
+
+                 options.Events = new OAuthEventHandler()
+                 {
+                     OnCreatingTicket = async context =>
+                     {
+                         var request = new HttpRequestMessage(HttpMethod.Get, context.Options.UserInformationEndpoint);
+                         request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                         request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", context.AccessToken);
+
+                         var response = await context.Backchannel.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, context.HttpContext.RequestAborted);
+                         response.EnsureSuccessStatusCode();
+
+                         var user = JObject.Parse(await response.Content.ReadAsStringAsync());
+
+                         context.RunClaimActions(user);
+                     }
+                 };
+             });
+
+            // -------------------------------------------------
+
             if (_environment.IsDevelopment())
             {
                 var dllPath = _configuration["StObjMap:Path"];
@@ -49,6 +101,18 @@ namespace API
             app.UseCors(builder =>
                 builder.WithOrigins("localhost:8080").AllowAnyHeader().AllowCredentials().AllowAnyMethod().AllowAnyOrigin());
             app.UseMvc();
+        }
+    }
+
+    internal class OAuthEventHandler : OAuthEvents
+    {
+        public override Task TicketReceived(TicketReceivedContext c)
+        {
+            var authService = c.HttpContext.RequestServices.GetRequiredService<WebFrontAuthService>();
+            return authService.HandleRemoteAuthentication<IUserGitHubInfo>(c, payload =>
+            {
+                payload.GitHubAccountId = c.Principal.FindFirst(ClaimTypes.NameIdentifier).Value;
+            });
         }
     }
 }
