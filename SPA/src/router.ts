2b2eff8e84eef277b63322cd9b85e44b068c5784
Pierre Viara <viara@intechinfo.fr>
2018-11-01 16:20:32 +0100

    Added NotFound component.

--- SPA/src/router.ts
+++ SPA/src/router.ts
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 
 // Components.
+import NotFound from './components/NotFound.vue';
 import Home from './components/Home.vue';
 import OrderPanel from './components/order-related/OLD-OrderStaffPanel.vue';
 
@@ -9,6 +10,12 @@ Vue.use(Router);
 
 export default new Router({
   routes: [
+    {
+      path: '*',
+      name: 'notFound',
+      component: NotFound,
+    },
+
     {
       path: '/',
       name: 'home',
