c4096ce771d00509176a554aa0fb1e79530995a7
Pierre Viara <viara@intechinfo.fr>
2018-11-05 18:33:57 +0100

    Added Storage table and its matching procedures.

---
+++ ITI.Human.Data/Res/Model/ITIH.StorageTable.Install.1.0.0.sql
@@ -0,0 +1,9 @@
+﻿create table ITIH.tStorage (
+	StorageId int not null identity(0, 1),
+	ProjectId int not null
+
+	constraint PK_tStorage_StorageId primary key (StorageId),
+	constraint FK_tStorage_ProjectId foreign key (ProjectId) references ITIH.tProject (ProjectId)
+)
+
+insert into ITIH.tStorage (ProjectId) values (0);
