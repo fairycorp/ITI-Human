3d5bfb9188932ce93bc3793e7ffa1ea7392eeed0
Pierre Viara <viara@intechinfo.fr>
2018-11-12 11:42:47 +0100

    Updated ProductService.

--- API/Services/Product/ProductService.cs
+++ API/Services/Product/ProductService.cs
@@ -1,4 +1,4 @@
-﻿using API.ViewModels.Product;
+﻿using ITI.Human.ViewModels.Product;
 using CK.SqlServer;
 using Dapper;
 using ITI.Human.Data;
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Threading.Tasks;
 
 using static API.Services.Helper.ResultFactory;
+using API.Services.Helper;
 
 namespace API.Services.Product
 {
@@ -93,9 +94,44 @@ namespace API.Services.Product
         {
             using (var ctx = new SqlStandardCallContext())
             {
+                // Checks if a Product already exists with this specific Product name.
+                // If does, returns Failure().
+                var doesProductExist =
+                    await Attempt.ToGetElement(GetProductByName, model.Name, false);
+
+                if (doesProductExist.Content == null) return Failure(doesProductExist.Info);
+
                 return Success(
                     await ProductTable.Create(ctx, 0, model.Name, model.Desc)
                 );
+            }
+        }
+
+        /// <summary>
+        /// Updates a specific Product.
+        /// </summary>
+        /// <param name="model">Product update view model.</param>
+        /// <returns>Success result where result content is true OR Failure result in case there's problem in update process.</returns>
+        public async Task<GuardResult> Updateproduct(UpdateViewModel model)
+        {
+            using (var ctx = new SqlStandardCallContext())
+            {
+                // Checks if a Product already exsists with this specific Product name.
+                // If does, returns Failure().
+                var doesProductExist =
+                    await Attempt.ToGetElement(GetProductByName, model.Name, false);
+
+                if (doesProductExist.Content == null) return Failure(doesProductExist.Info);
+
+                // Retrieves current product to fulfill model missing properties.
+                var currentProduct = doesProductExist.Content;
+                model.Name = model.Name ?? ((BasicDataProduct)currentProduct).Name;
+                model.Desc = model.Desc ?? ((BasicDataProduct)currentProduct).Desc;
+
+                // Launches update process.
+                var result = await ProductTable.Update(ctx, 0, model.ProductId, model.Name, model.Desc);
+                if (result == false) return Failure("Error in update process.");
+                return Success(result);
             }
         }
     }
---
+++ ITI.Human.ViewModels/Product/UpdateViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace ITI.Human.ViewModels.Product
+{
+    public class UpdateViewModel
+    {
+        /// <summary>
+        /// See <see cref="BasicDataProduct.ProductId"/>.
+        /// </summary>
+        public int ProductId { get; set; }
+
+        /// <summary>
+        /// See <see cref="BasicDataProduct.Name"/>.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// See <see cref="BasicDataProduct.Desc"/>.
+        /// </summary>
+        public string Desc { get; set; }
+    }
+}
