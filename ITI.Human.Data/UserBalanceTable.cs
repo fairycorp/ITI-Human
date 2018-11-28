b53c942eb1623ef3c33d7851454c9e2720e5adfa
Dizy903 <Goffart@intechinfo.fr>
2018-11-28 15:34:19 +0100

    Initialized UserBalanceTable creation process(C# part).

---
+++ ITI.Human.Data/UserBalanceTable.cs
@@ -0,0 +1,19 @@
+﻿using CK.DB.Actor;
+using CK.Setup;
+using CK.SqlServer.Setup;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ITI.Human.Data
+{
+    [SqlTable("tUserBalance", Package = typeof(Package))]
+    [Versions("1.0.0")]
+    public abstract class UserBalanceTable : SqlTable
+    {
+        void StObjConstruct(UserTable uTable)
+        {
+
+        }
+    }
+}
