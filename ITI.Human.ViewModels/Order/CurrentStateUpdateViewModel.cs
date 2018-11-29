5effdde6094449ccb2996a5bce867feea6b1da1e
Dizy903 <Goffart@intechinfo.fr>
2018-11-29 10:59:29 +0100

    Added CurrentStateUpdateViewModel

---
+++ ITI.Human.ViewModels/Order/CurrentStateUpdateViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static ITI.Human.ViewModels.Product.Ordered.BasicDataOrderedProduct;
+
+namespace ITI.Human.ViewModels.Order
+{
+    public class CurrentStateUpdateViewModel
+    {       
+        public int OrdererProductId { get; set; }
+
+        public State CurrentState { get; set; }
+    }
+}
