f6467fe5e12cac16616b005bf091efa4f208527b
Pierre Viara <viara@intechinfo.fr>
2018-11-30 17:27:45 +0100

    Reworked and documented UserService methods.

--- API/Services/User/UserService.cs
+++ API/Services/User/UserService.cs
@@ -3,10 +3,11 @@ using CK.SqlServer;
 using Dapper;
 using ITI.Human.ViewModels.User;
 using Stall.Guard.System;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
+using static API.Services.Helper.ResultFactory;
 
 namespace API.Services.User
 {
@@ -20,60 +21,69 @@ namespace API.Services.User
         }
 
         /// <summary>
+        /// Gets all Users from database.
+        /// </summary>
+        /// <returns>
+        /// Success result where result content is a list of <see cref="BasicDataUser"/>
+        /// or Failure result if no element exists in db.
+        /// </returns>
+        public async Task<GuardResult> GuardedGetAll()
+        {
+            using (var ctx = new SqlStandardCallContext())
+            {
+                var result = await GetAll();
+
+                if (result == null) return Failure("No element was found.");
+                return Success(result);
+            }
+        }
+
+        /// <summary>
         /// Gets a User by its id.
         /// </summary>
-        /// <param name="userId">Classroom id.</param>
-        private async Task<UserBasicData> GetUser(int userId)
+        /// <param name="userId">User's id.</param>
+        /// <returns>
+        /// Success result where result content is a single <see cref="BasicDataUser"/> 
+        /// or Failure result if element does not exist in db.
+        /// </returns>
+        public async Task<GuardResult> GuardedGet(int userId)
+        {
+            var result = await Get(userId);
+
+            if (result == null) return Failure("No element was found.");
+            return Success(result);
+        }
+
+        // --------------------------------------------------------------------------------------------
+
+        private async Task<IEnumerable<BasicDataUser>> GetAll()
+        {
+            using (var ctx = new SqlStandardCallContext())
+            {
+                return (await ctx[UserTable].Connection
+                    .QueryAsync<BasicDataUser>(
+                        @"SELECT
+                            *
+                        FROM 
+                            CK.tUser;"
+                    )).ToArray();
+            }
+        }
+
+        private async Task<BasicDataUser> Get(int userId)
         {
             using (var ctx = new SqlStandardCallContext())
             {
                 return await ctx[UserTable].Connection
-                    .QueryFirstOrDefaultAsync<UserBasicData>(
+                    .QueryFirstOrDefaultAsync<BasicDataUser>(
                         @"SELECT
-                            UserId
+                            *
                         FROM
                             CK.tUser
                         WHERE
-                            UserId = @id",
+                            UserId = @id;",
                         new { id = userId }
                     );
-            }
-        }
-
-        /// <summary>
-        /// Gets all Projects from database.
-        /// </summary>
-        public async Task<GuardResult> GetAllUsers()
-        {
-            using (var ctx = new SqlStandardCallContext())
-            {
-                var result = await ctx[UserTable].Connection
-                    .QueryAsync<UserBasicData>(
-                         @"SELECT
-                            *
-                        FROM 
-                            CK.tUser;"
-                    );
-                if (result == null) return Failure("No element was found.");
-                return Success(result.ToArray());
-            }
-        }
-
-        /// <summary>
-        /// Gets all Users from database.
-        /// </summary>
-        public async Task<IEnumerable<UserBasicData>> GetAllUsers()
-        {
-            using (var ctx = new SqlStandardCallContext())
-            {
-                var result = await ctx[UserTable].Connection
-                .QueryAsync<UserBasicData>(
-                    @"SELECT
-                        *
-                    FROM
-                        CK.tUser;"
-                );
-                return result.ToArray();
             }
         }
     }
