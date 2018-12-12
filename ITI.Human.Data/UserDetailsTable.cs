14dea4a49bcc34bafeb4bcfe43b53778e54362ba
Pierre Viara <viara@intechinfo.fr>
2018-12-12 15:43:45 +0100

    Added UserDetailsTable.

---
+++ ITI.Human.Data/UserDetailsTable.cs
@@ -0,0 +1,15 @@
+﻿using CK.DB.Actor;
+using CK.Setup;
+using CK.SqlServer.Setup;
+
+namespace ITI.Human.Data
+{
+    [SqlTable("tUserDetails", Package = typeof(Package))]
+    [Versions("1.0.0")]
+    public abstract class UserDetailsTable : SqlTable
+    {
+        void StObjConstruct(UserTable uTable)
+        {
+        }
+    }
+}
