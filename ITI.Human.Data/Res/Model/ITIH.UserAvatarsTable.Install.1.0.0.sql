37f50ad743db3d3434b6930e3412d2144e860a3f
Pierre Viara <viara@intechinfo.fr>
2018-12-17 10:13:46 +0100

    Added UserAvatarsTable.

---
+++ ITI.Human.Data/Res/Model/ITIH.UserAvatarsTable.Install.1.0.0.sql
@@ -0,0 +1,10 @@
+﻿create table ITIH.tUserAvatars (
+	UserAvatarId int not null identity(0, 1),
+	UserId int not null,
+	[Image] varbinary(8000) not null
+
+	constraint PK_tUserAvatars_UserAvatarId primary key (UserAvatarId),
+	constraint FK_tUserAvatars_UserId foreign key (UserId) references CK.tUser
+);
+
+insert into ITIH.tUserAvatars (UserId, [Image]) values (0, 0x);
---
+++ ITI.Human.Data/UserAvatarsTable.cs
@@ -0,0 +1,21 @@
+﻿using CK.DB.Actor;
+using CK.Setup;
+using CK.SqlServer;
+using CK.SqlServer.Setup;
+using System;
+using System.Threading.Tasks;
+
+namespace ITI.Human.Data
+{
+    [SqlTable("tUserAvatars", Package = typeof(Package))]
+    [Versions("1.0.0")]
+    public abstract class UserAvatarsTable : SqlTable
+    {
+        void StObjConstruct(UserTable uTable)
+        {
+        }
+
+        [SqlProcedure("sUserAvatarCreate")]
+        public abstract Task<int> Create(ISqlCallContext ctx, int actorId, int userId, byte[] image);
+    }
+}
